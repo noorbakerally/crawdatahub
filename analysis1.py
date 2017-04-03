59f32b53b415f0dd0be433e24dd4720da8189b78
Noorani Bakerally <noorani.bakerally@gmail.com>
2017-04-03 15:29:15 +0200

    printing summary table

--- analysis1.py
+++ analysis1.py
@@ -1,5 +1,30 @@
 import json
 from rdflib import Graph
+from texttable import Texttable
+
+def printStable(sArray):
+	print "Triple Category"
+	print "==============="
+	t = Texttable()
+	t.add_rows([['Rep', 'ST','OT',"NumCase","PNumCase"]])	
+	for rep in sArray.keys():
+		crep = sArray[rep]
+		pNumCase = (float(crep["NumCase"])/totalCases)*100
+		t.add_row([rep,crep["ST"],crep["OT"],crep["NumCase"],pNumCase])	
+	t.add_row(['','','Total',totalCases,''])
+	print t.draw()
+
+def getPopularSubject(g):
+	query = '''select distinct ?s (COUNT(?s) as ?numS ) WHERE {
+		   ?s ?p ?o 
+		} 
+		GROUP BY ?s 
+		ORDER BY DESC (?numS)
+		LIMIT 1'''
+	result = g.query(query)
+	for subject in result:
+		return subject.s
+
 
 def getSubjects(g):
         query = "SELECT DISTINCT ?s WHERE { { ?s ?x ?o . } UNION { ?o ?y ?s .} FILTER (isIRI(?s)) }"
@@ -28,30 +53,54 @@ filesDirectory = "/home/bakerally/Documents/repositories/emse_gitlab/crawdatahub
 
 numLinkInSubject = 0
 indirectionPred = []
+totalCases = 0
+
+sCases = {"GT":{"ST":"=0","OT":"=0","NumCase":0,"PNumCase":7},
+	  "ST":{"ST":">0","OT":"=0","NumCase":0,"PNumCase":7},
+	  "SOT":{"ST":">0","OT":">0","NumCase":0,"PNumCase":7},
+	  "OT":{"ST":"=0","OT":">0","NumCase":0,"PNumCase":7} }
+
 for result in results.keys():
+	cresult = results[result]
+	if cresult["ATriples"] == "0":
+		continue
+	
+	totalCases = totalCases + 1
 	index = links["links"][result]		
 	rformat = rlinkstatus[result]
-	if "http://el.dbpedia.org/data/Linux.rdf" not in result:
-		continue
-	g = Graph()
-	g = g.parse(filesDirectory+str(index),format=rformat)
-	cresult = results[result]
+	#g = Graph()
+	#g = g.parse(filesDirectory+str(index),format=rformat)
 
-	subjects = getSubjects(g)
-        for subject in subjects:
-		predicates = getIndirectionPredicates(g,subject.s,result)
-		if len(predicates) > 0:
-			print predicates
-	break
 	if cresult["STriples"] == "0" and cresult["OTriples"] == "0":
-		print "test"
+		sCases["GT"]["NumCase"] = sCases["GT"]["NumCase"] + 1			
+
+	elif cresult["STriples"] != "0" and cresult["OTriples"] == "0":
+		sCases["ST"]["NumCase"] = sCases["ST"]["NumCase"] + 1			
+	
+	elif cresult["STriples"] != "0" and cresult["OTriples"] != "0":
+		sCases["SOT"]["NumCase"] = sCases["SOT"]["NumCase"] + 1			
+	
+	elif cresult["STriples"] == "0" and cresult["OTriples"] != "0":
+		sCases["OT"]["NumCase"] = sCases["OT"]["NumCase"] + 1			
 
 
-		subjects = getSubjects(g)
-		for subject in subjects:
-			if subject.s in result:
-				sName = subject.s
-				numLinkInSubject = numLinkInSubject + 1
-				break
-		break
-print numLinkInSubject
+printStable(sCases)	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

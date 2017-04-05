7af1a438ba498389a388fd4fa50e499110add8da
Noorani Bakerally <noorani.bakerally@gmail.com>
2017-04-05 11:52:07 +0200

    before merge

--- generateRDF.py
+++ generateRDF.py
@@ -1,5 +1,8 @@
 import json
 from rdflib import Graph, Literal, BNode, Namespace, RDF, URIRef
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
 
 with open('rRepStats.json', 'r') as infile:
     data = json.load(infile)
@@ -11,20 +14,21 @@ g.bind("on",ON)
 g.bind("da",DA)
 
 for rlink in data:
+	#print rlink
 	rlink = rlink.replace("\n","")
 	aTriples = data[rlink]["ATriples"]
 	sTriples = data[rlink]["STriples"]
 	oTriples = data[rlink]["OTriples"]
-	oTriples = data[rlink]["GTriples"]
+	gTriples = data[rlink]["GTriples"]
 	linkNode = URIRef(rlink)	
 	aTripleNode = URIRef(on+"nbATriples")
 	sTripleNode = URIRef(on+"nbSTriples")
 	oTripleNode = URIRef(on+"nbOTriples")
 	gTripleNode = URIRef(on+"nbGTriples")
-
+	
 	g.add( (linkNode, aTripleNode, Literal(int(aTriples))) )
 	g.add( (linkNode, sTripleNode, Literal(int(sTriples))) )
 	g.add( (linkNode, oTripleNode, Literal(int(oTriples))) )
 	g.add( (linkNode, gTripleNode, Literal(int(gTriples))) )
-
+	#break
 print g.serialize(format="turtle")

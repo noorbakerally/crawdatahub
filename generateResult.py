0037a04e94ee3bbafae9d497534e3d65593f7286
Noorani Bakerally <noorani.bakerally@gmail.com>
2017-04-07 11:31:28 +0200

    fix_error

--- generateResult.py
+++ generateResult.py
@@ -1,6 +1,9 @@
 import json
 import traceback
 from rdflib import Graph
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
 
 def getSNumTriples(g,resource):
         query = "SELECT (count(*) as ?numS) WHERE { <"+resource+"> ?x ?o}"
@@ -77,8 +80,8 @@ errors = {}
 rRep = {}
 
 counter = 0
-#for rlink in cleanLinks["links"].keys():
-for rlink in ["http://aims.fao.org/aos/data/c_2724?output=xml","http://aemet.linkeddata.es/resource/WeatherStation/id08001?output=ttl"]:
+for rlink in cleanLinks["links"].keys():
+#for rlink in ["http://aims.fao.org/aos/data/c_2724?output=xml","http://aemet.linkeddata.es/resource/WeatherStation/id08001?output=ttl"]:
 	#print rlink
 	counter = counter + 1
 	i = cleanLinks["links"][rlink]	
@@ -116,7 +119,7 @@ for rlink in ["http://aims.fao.org/aos/data/c_2724?output=xml","http://aemet.lin
 					rRep[rlink]["PResourceDetails"]["OTriples"] = OTriples
 					rRep[rlink]["PResourceDetails"]["GTriples"] = GTriples
 					rRep[rlink]["PResourceDetails"]["Relations"] = getRelationWithResource(g,pResource,rlink)
-			print "success:" + str(counter)
+		print "success:" + rlink+" "+str(counter)
 		
 	except:
 		print "error:"
